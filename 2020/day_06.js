67059b4549ff0142750db6202031c0c299736a4e
ComaToes <comatoes@blerf.net>
2020-12-07 20:31:20 +0000

    Update day_06.js

--- 2020/day_06.js
+++ 2020/day_06.js
@@ -14,6 +14,10 @@ a
 
 b`;
 
-const count = data.split("\n\n").map( x => x.replace(/\W/g,"") ).map( x => x.split("").reduce( (a,x) => a[x]=x && a, {} ) ).map( x => Object.keys(x).length ).reduce( (a,x) => a+x );
+const part1 = data.split("\n\n").map( x => x.replace(/\W/g,"") ).map( x => x.split("").reduce( (a,x) => a[x]=x && a, {} ) ).map( x => Object.keys(x).length ).reduce( (a,x) => a+x );
 
-console.log( count );
+console.log( part1 );
+
+const part2 = data.split("\n\n").map( x => ({ str: x.replace(/\W/g,""), count: x.replace(/\w/g,"").length+1}) ).map( ({count,str}) => ({ map: str.split("").reduce( (a,x) => (a[x]=(a[x]||0)+1) && a, {} ), count }) ).map( ({map,count}) => Object.keys(map).filter( k => map[k] == count ).length ).reduce( (a,x) => a+x );
+
+console.log( part2 );
