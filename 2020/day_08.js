4044f432d3862353fe30125f339175e3c32719a6
ComaToes <comatoes@blerf.net>
2020-12-08 09:08:10 +0000

    Update day_08.js

--- 2020/day_08.js
+++ 2020/day_08.js
@@ -12,6 +12,6 @@ const part1 = data.split("\n").map( x => ([op,val]=x.split(" ")) && {op,val:Numb
 
 console.log( part1 );
 
-const part2 = data.split("\n").map( x => ([op,val]=x.split(" ")) && {op,val:Number(val)} ).map((x,i,arr)=> arr.map( ({op,val},j) => ({op: i!=j?op: op == "nop"?"jmp": op=="jmp"?"nop":op,val}) )).map( x=> x.reduce( ({pc,r,h,done,finished},x,i,arr) => done ? {pc,r,h,done,finished} : pc == arr.length ? {pc,r,h,done: true, finished: true,arr} : h.includes(pc) ? {pc,r,h,done: true} : ({pc: pc + (arr[pc].op == "jmp" ? arr[pc].val : 1), r: r + (arr[pc].op == "acc" ? arr[pc].val : 0), h: h.concat([pc])}), {pc: 0, r: 0, h: []}) ).filter( x => x.finished )[0].r;
+const part2 = data.split("\n").map( x => ([op,val]=x.split(" ")) && {op,val:Number(val)} ).map((x,i,arr)=> arr.map( ({op,val},j) => ({op: i!=j?op: op == "nop"?"jmp": op=="jmp"?"nop":op,val}) )).map( x=> x.reduce( ({pc,r,h,done,finished},x,i,arr) => done ? {pc,r,h,done,finished} : pc == arr.length ? {pc,r,h,done: true, finished: true} : h.includes(pc) ? {pc,r,h,done: true} : ({pc: pc + (arr[pc].op == "jmp" ? arr[pc].val : 1), r: r + (arr[pc].op == "acc" ? arr[pc].val : 0), h: h.concat([pc])}), {pc: 0, r: 0, h: []}) ).filter( x => x.finished )[0].r;
 
 console.log( part2 );
