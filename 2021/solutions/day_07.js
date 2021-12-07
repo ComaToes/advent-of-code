3de36e41ed2464ab53e8a50c1f0c14f5f41e98d7
ComaToes <comatoes@blerf.net>
2021-12-07 14:35:09 +0000

    2021 Day 7 More Better

--- 2021/solutions/day_07.js
+++ 2021/solutions/day_07.js
@@ -1,4 +1,4 @@
-function findLeastFuel(crabs, costFn) {
+function findLeastFuelNaive(crabs, costFn) {
 
     const [min, max] = crabs.reduce( 
         ([min, max], crab) => [Math.min(min, crab), Math.max(max, crab)], 
@@ -16,17 +16,48 @@ function findLeastFuel(crabs, costFn) {
 
 }
 
+function findLeastFuelBinary(crabs, costFn) {
+
+    const [min, max] = crabs.reduce( 
+        ([min, max], crab) => [Math.min(min, crab), Math.max(max, crab)], 
+        [Number.MAX_SAFE_INTEGER, 0] );
+
+    let bmin = min, bmax = max;
+
+    let leastFuel = 0;
+    while( bmin != bmax ) {
+        const mid = Math.floor( (bmin + bmax) / 2 );
+        const fuelA = crabs.reduce( (fuel, crab) => fuel + costFn(crab, mid), 0 );
+        const fuelB = crabs.reduce( (fuel, crab) => fuel + costFn(crab, mid+1), 0 );
+        if( fuelA < fuelB ) {
+            bmax = mid;
+            leastFuel = fuelA;
+        } else {
+            bmin = mid + 1;
+            leastFuel = fuelB;
+        }
+    }
+
+    return leastFuel;
+
+}
+
 function part1(data) {
 
     const crabs = data.trim().split(",").map(Number);
-    return findLeastFuel( crabs, (a, b) => Math.abs(a - b) );
+
+    crabs.sort( (a, b) => a - b );
+
+    const median = crabs[ Math.floor(crabs.length / 2) ];
+
+    return crabs.reduce( (fuel, crab) => fuel + Math.abs(crab - median), 0 );
 
 }
 
 function part2(data) {
 
     const crabs = data.trim().split(",").map(Number);
-    return findLeastFuel( crabs, (a, b) => {
+    return findLeastFuelBinary( crabs, (a, b) => {
         const d = Math.abs(a - b);
         return d / 2 * (1 + d);
     } );
