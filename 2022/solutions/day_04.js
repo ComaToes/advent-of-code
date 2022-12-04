7ab5c92265b199b5e560a3d994c7220d9bd25c5d
ComaToes <comatoes@blerf.net>
2022-12-04 05:25:37 +0000

    2022 Day 4

---
+++ 2022/solutions/day_04.js
@@ -0,0 +1,29 @@
+function part1(data) {
+
+    const pairs = data.split(/\r?\n/).map( 
+        line => line.split(",").map( range => range.split("-").map(Number) )
+    )
+
+    const contains = (a,b) => a[0] >= b[0] && a[0] <= b[1] && a[1] >= b[0] && a[1] <= b[1]
+
+    const contained = pairs.filter( ([a,b]) => contains(a,b) || contains(b,a) )
+
+    return contained.length
+
+}
+
+function part2(data) {
+
+    const pairs = data.split(/\r?\n/).map( 
+        line => line.split(",").map( range => range.split("-").map(Number) )
+    )
+
+    const overlaps = (a,b) => a[0] >= b[0] && a[0] <= b[1] && a[1] >= b[0]
+
+    const overlapped = pairs.filter( ([a,b]) => overlaps(a,b) || overlaps(b,a) )
+
+    return overlapped.length
+
+}
+
+module.exports = { part1, part2 }
