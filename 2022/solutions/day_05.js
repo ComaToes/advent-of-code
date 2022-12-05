9aad8820de988924e5bfa32ab66e0537dbb57988
ComaToes <comatoes@blerf.net>
2022-12-05 07:33:56 +0000

    2022 Day 5

---
+++ 2022/solutions/day_05.js
@@ -0,0 +1,53 @@
+function parseData(data) {
+
+    const [rawState, rawMoves] = data.split(/\r?\n\r?\n/)
+
+    const stateLines = rawState.split(/\r?\n/)
+    const stackCount = (stateLines.pop().trim().length + 3) / 4
+
+    const stacks = [...Array(stackCount).keys()].map( () => [] )
+
+    stateLines.forEach( line => {
+        for (let i = 0; i < stackCount; i++) {
+            const ch = line[i*4+1]
+            if( ch != " " )
+                stacks[i].unshift(ch)
+        }
+    })
+    
+    const moves = rawMoves.split(/\r?\n/).map( move => {
+        const match = move.match(/move (\d+) from (\d+) to (\d+)/)
+        const [count,from,to] = match.slice(1,4).map(Number)
+        return {count,from,to}
+    })
+
+    return {stacks, moves}
+}
+
+function part1(data) {
+
+    const {stacks, moves} = parseData(data)
+
+    moves.forEach( ({count, from, to}) => {
+        const elems = stacks[from-1].splice( -count, count )
+        stacks[to-1].push(...(elems.reverse()))
+    })
+
+    return stacks.map( s => s.pop() ).join('')
+
+}
+
+function part2(data) {
+
+    const {stacks, moves} = parseData(data)
+
+    moves.forEach( ({count, from, to}) => {
+        const elems = stacks[from-1].splice( -count, count )
+        stacks[to-1].push(...elems)
+    })
+
+    return stacks.map( s => s.pop() ).join('')
+
+}
+
+module.exports = { part1, part2 }
